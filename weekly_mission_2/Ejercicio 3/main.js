123b4196ff275c2a17937d8d5b5ed82914b16e7e
FranciscoChavarria <fjchf.1980@gmail.com>
2022-04-16 20:15:02 -0500

    commit Ejercicio 3 weekly 2

---
+++ weekly_mission_2/Ejercicio 3/main.js
@@ -0,0 +1,8 @@
+const Repo = require('./def_class') // Invocas el módulo que contiene la clase
+
+// Creación de un objeto
+const repo1 = new Repo("LaunchX","Francisco Chavarria", "JavaScript",100,199,299,10,10) // Creas un objeto nuevo, esto llama por default el constructor de la clase
+
+// invocación del método
+repo1.getTotalIssues();
+repo1.getGeneralInfo();
