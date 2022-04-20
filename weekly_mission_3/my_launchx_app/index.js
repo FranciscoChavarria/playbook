40ba5df9f38d3225037a5bb1ccd648b43672d461
FranciscoChavarria <fjchf.1980@gmail.com>
2022-04-20 16:58:22 -0500

    Creación de carpetas app y test, index con clase MissionComander

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+const paco = new MissionCommander("Paco")
+const ramon = new MissionCommander("Ramon")
+const bety = new MissionCommander("Bety")
+
+console.log(paco.name)
+console.log(ramon.name)
+console.log(bety.name)
