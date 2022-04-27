6f2895cab31e83711dc4d9d66fd3fa882984b353
FranciscoChavarria <fjchf.1980@gmail.com>
2022-04-27 14:13:31 -0500

    Se agregan pruebas Jest validas

---
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -0,0 +1,12 @@
+class Spiderman{
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
+
+    getInfo = function(){
+        return "Hey, I'm Tom Holland from Marvel studio";
+    }
+}
+
+module.exports = Spiderman
--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,7 +1,14 @@
+const Spiderman = require("./../app/spiderman")
+
 describe("Unit test for spiderman class", () => {
     test('1) Create an spoderman object', () => {
       const andrewGarfield = new Spiderman("Spiderman Sony", 31)
       expect(andrewGarfield.name).toBe("Spiderman Sony");
       expect(andrewGarfield.age).toBe(31);
+    });
+
+    test('2) Get Spiderman Studio', () => {
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31)
+      expect(andrewGarfield.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
     });
 })
