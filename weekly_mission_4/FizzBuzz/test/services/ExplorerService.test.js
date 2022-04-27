0deb510d5a5371048fdc7bd55e2775f366e126d4
FranciscoChavarria <fjchf.1980@gmail.com>
2022-04-27 16:45:37 -0500

    Paso 9: Fix Prueba fallida cantidad de explorers

--- weekly_mission_4/FizzBuzz/test/services/ExplorerService.test.js
+++ weekly_mission_4/FizzBuzz/test/services/ExplorerService.test.js
@@ -2,7 +2,7 @@ const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Test para ExplorerService", () => {
     test('1) Calcular numero de explorer en mission: "node"', () => {
-      const explorers = [{mission:'node'},{mission:'node'}]
+      const explorers = [{mission:'node'}]
       const cantidad = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
       expect(cantidad).toBe(1);
     });
