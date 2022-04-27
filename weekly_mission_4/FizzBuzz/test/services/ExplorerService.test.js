aa0e62532858a90479e8b414dbe0017f819288b6
FranciscoChavarria <fjchf.1980@gmail.com>
2022-04-27 17:02:48 -0500

    Paso 10: Agregando prueba getExplorersUsernamesByMission

--- weekly_mission_4/FizzBuzz/test/services/ExplorerService.test.js
+++ weekly_mission_4/FizzBuzz/test/services/ExplorerService.test.js
@@ -6,4 +6,13 @@ describe("Test para ExplorerService", () => {
       const cantidad = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
       expect(cantidad).toBe(1);
     });
+
+    test('2) Mostrar nombres explorers en mission: "node"', () => {
+        const explorers = [{mission:'node',githubUsername:'Mario'}]
+        const nombres = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        const resultExpected = ["Mario"]
+        console.log(nombres)
+        console.log(resultExpected)
+        expect(nombres[0]).toBe(resultExpected[0]);
+      });
 })
